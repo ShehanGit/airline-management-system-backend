484876c1b6edb19e41c27f6aabc140dfbaca1537
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2025-03-15 21:38:10 +0530

    ss
    ss

--- src/main/java/com/airlinemanagementsystem/airline_management_system/AirlineManagementSystemApplication.java
+++ src/main/java/com/airlinemanagementsystem/airline_management_system/AirlineManagementSystemApplication.java
@@ -12,7 +12,7 @@ public class AirlineManagementSystemApplication {
 
 	public static void main(String[] args) {
 
-		
+
 		SpringApplication.run(AirlineManagementSystemApplication.class, args);
 	}
 
