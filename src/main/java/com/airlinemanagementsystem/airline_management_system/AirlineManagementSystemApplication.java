f89c40ea29478024f8e9377daba2aba9d2b10851
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2025-03-16 20:40:24 +0530

    ss

--- src/main/java/com/airlinemanagementsystem/airline_management_system/AirlineManagementSystemApplication.java
+++ src/main/java/com/airlinemanagementsystem/airline_management_system/AirlineManagementSystemApplication.java
@@ -2,19 +2,14 @@ package com.airlinemanagementsystem.airline_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.airlinemanagementsystem.airline_management_system.config.JwtService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AirlineManagementSystemApplication {
 
 	public static void main(String[] args) {
 
-
 		SpringApplication.run(AirlineManagementSystemApplication.class, args);
 	}
 
-	
+
 }
